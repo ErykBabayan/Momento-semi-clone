4ee284b1e6d8c6e19bc20569626116295563cec6
ErykBabayan <74492818+ErykBabayan@users.noreply.github.com>
2022-01-10 23:34:11 +0100

    added error handling with displaying default img when error occurs

--- project files/script.js
+++ project files/script.js
@@ -1,10 +1,20 @@
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
-	.then((response) => response.json())
+	.then((response) => {
+		if (!response.ok) {
+			console.log(response.status);
+			throw Error("Something went wrong");
+		}
+		return response.json();
+	})
 	.then((data) => {
 		console.log(data);
 		if (window.innerWidth < 1000) {
 			document.body.style.backgroundImage = `url(${data.urls.regular})`;
 		} else {
-			document.body.style.backgroundImage = `url(${data.urls.full})`;
+			document.body.style.backgroundImage = `url(${data.urls.regular})`;
 		}
+	})
+	.catch((err) => {
+		document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1466133633688-187f0b492390?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NDE4NDk0NzI&ixlib=rb-1.2.1&q=85)`;
+		photoAuthor.textcontent = "Jean-Pierre Brungs";
 	});
