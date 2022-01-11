c9aaf7cebf00d5540e38ea075072732c0c6f9763
ErykBabayan <74492818+ErykBabayan@users.noreply.github.com>
2022-01-11 20:39:02 +0100

    Added conditions that add 0 to current time if time is less than 10

--- project files/scripts/timeHandler.js
+++ project files/scripts/timeHandler.js
@@ -1,8 +1,25 @@
 const clock = document.getElementById("clock");
-const date = new Date();
 const interval = 1000;
 
+let hours;
+let minutes;
+
 setInterval(() => {
-	currentTime = `${date.getHours()} : ${date.getMinutes()} `;
+	const date = new Date();
+	hours = "0" + date.getHours();
+	minutes = "0" + date.getMinutes();
+	currentTime = `${date.getHours()} : ${date.getMinutes()}`;
+
+	//adds a 0 before hours/mins if they are less than 10 ( for better visuals)
+	if (date.getHours() < 10) {
+		currentTime = `${hours} : ${date.getMinutes()}`;
+	}
+	if (date.getMinutes() < 10) {
+		currentTime = `${date.getHours()} : ${minutes}`;
+	}
+	if (date.getHours() < 10 && date.getMinutes() < 10) {
+		currentTime = `${hours} : ${minutes}`;
+	}
+
 	clock.textContent = currentTime;
 }, interval);
